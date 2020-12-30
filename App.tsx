2ecefc0276d166b0f26716db59414a68bc92e151
ocbruno <brunocgmxus@gmail.com>
2020-12-30 19:29:30 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -4,7 +4,9 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.userFullNameHeader}>Bruno Costa</Text>
+      <Text style={styles.userBiography}>Frontend Web/Mobile Developer</Text>
+      <Text style={styles.userQualifications}>Html5, CSS3, Javascript, React</Text>
     </View>
   );
 }
@@ -12,8 +14,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(255, 255, 255)',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  userFullNameHeader: {
+    color: '#0f0404',
+    fontSize: 24
+  },
+  userBiography: {
+    color: '#0f0404',
+    fontSize: 12
+  },
+  userQualifications: {
+    color: '#0f0404',
+    fontSize: 9
+  }
 });
